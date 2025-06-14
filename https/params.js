34a08ca051efe3c516cd66d05da3481febb6a5e9
VictorG411 <vguzman2@students.columbiabasin.edu>
2025-06-13 23:24:29 -0700

    Create params.js

---
+++ https/params.js
@@ -0,0 +1,6 @@
+// AWS Params
+const bucketName = "";
+const bucketRegion = ""; //i.e. us-west-2
+const identityPoolId = "";
+const idProvider = ""; // i.e. cognito-idp.REGION.amazonaws.com/PROVIDER
+const cognitoLoginURL = "";
